a7e580f27455f8dd750c3f01ed35f657f7fb3c15
tashi1118 <tech.tashi1118.1991@gmail.com>
2022-02-23 19:10:28 +0900

    alb ターゲットグループとテスト用リスナー作成

--- alb/main.tf
+++ alb/main.tf
@@ -42,6 +42,64 @@ module "security_group_for_alb" {
 }
 
 #--------------------------------------------------
+# Target group
+#--------------------------------------------------
+
+data "aws_instance" "web_ec2_1a" {
+  instance_tags = {
+    ProjectName  = "terraform-template"
+    Environment  = "dev"
+    ResourceName = "ec2-1a"
+  }
+}
+
+data "aws_instance" "web_ec2_1c" {
+  instance_tags = {
+    ProjectName  = "terraform-template"
+    Environment  = "dev"
+    ResourceName = "ec2-1c"
+  }
+}
+
+resource "aws_lb_target_group" "tg_ec2" {
+  name     = "${local.namePrefix}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.network.vpc_id
+
+  health_check {
+    protocol            = "HTTP"
+    path                = "/"
+    port                = "80"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 100
+    matcher             = 200
+  }
+
+  tags = {
+    Name         = "${local.namePrefix}-tg-ec2"
+    Environment  = local.environment
+    ProjectName  = local.projectName
+    ResourceName = "tg-ec2"
+    Tool         = local.toolName
+  }
+}
+
+resource "aws_lb_target_group_attachment" "attach_web_ec2_1a" {
+  target_group_arn = aws_lb_target_group.tg_ec2.arn
+  target_id        = data.aws_instance.web_ec2_1a.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "attach_web_ec2_1c" {
+  target_group_arn = aws_lb_target_group.tg_ec2.arn
+  target_id        = data.aws_instance.web_ec2_1c.id
+  port             = 80
+}
+
+#--------------------------------------------------
 # ALB
 #--------------------------------------------------
 
@@ -79,18 +137,84 @@ resource "aws_lb" "alb" {
   }
 }
 
-resource "aws_lb_listener" "http" {
+#--------------------------------------------------
+# Listener
+#--------------------------------------------------
+
+resource "aws_lb_listener" "alb_http_listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
+    target_group_arn = aws_lb_target_group.tg_ec2.arn
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_listener_rule" "redirect_google" {
+  listener_arn = aws_lb_listener.alb_http_listener.arn
+
+  action {
+    type = "redirect"
+
+    redirect {
+      host        = "google.co.jp"
+      port        = "443"
+      path        = "/"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = ["/google"]
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "fixed_response" {
+  listener_arn = aws_lb_listener.alb_http_listener.arn
+
+  action {
     type = "fixed-response"
 
     fixed_response {
       content_type = "text/plain"
       message_body = "これはHTTPです"
       status_code  = "200"
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = ["/test"]
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "health_check" {
+  listener_arn = aws_lb_listener.alb_http_listener.arn
+
+  action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "HEALTHY"
+      status_code  = "200"
+    }
+  }
+
+  condition {
+    query_string {
+      key   = "health"
+      value = "check"
+    }
+
+    query_string {
+      value = "bar"
     }
   }
 }
